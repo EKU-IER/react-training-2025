3207e274c29f9360dd7115779a0118c3092ae135
chamce <chancellor.whitaker@eku.edu>
2025-01-14 12:07:27 -0500

    implemented search box & lifted state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,73 @@
+import { useState, useId } from "react";
+
+import { usePromise } from "./usePromise";
 import GridExample from "./GridExample";
 import Wrapper from "./Remote";
-import { usePromise } from "./usePromise";
 
 const reportsUrl =
   "https://raw.githubusercontent.com/chancellor-w-whitaker/json-example/refs/heads/main/reports.json";
 
 const reportsPromise = fetch(reportsUrl).then((response) => response.json());
 
+// create dropdown filters with a different dataset
+
+// implement search with data page dataset
+
 export default function App() {
+  const [search, setSearch] = useState("");
+
   const reports = usePromise(reportsPromise);
 
-  const firstReport = Array.isArray(reports) ? reports[0] : {};
+  const columnDefs = createColumnDefs(reports);
 
-  const fields = Object.keys(firstReport);
-
-  // fields
-  // ["field1", "field2", ..., "fieldX"]
-
-  const columnDefs = fields.map((field) => ({ field }));
-
-  // make table
-  // components: functions that return markup (html), parameters could be anything
-  // find table library: search google (ag grid)
+  // filter reports by search
+  // search should be based on report descriptions
+  // handle empty search differently
 
   return (
-    <Wrapper>
-      <GridExample rowData={reports} columnDefs={columnDefs}></GridExample>
+    <Wrapper heading={"Search example"}>
+      <SearchBox
+        onChange={setSearch}
+        label="Description"
+        value={search}
+      ></SearchBox>
+      <GridExample columnDefs={columnDefs} rowData={reports}></GridExample>
     </Wrapper>
   );
+}
+
+function SearchBox({
+  placeholder = "Placeholder",
+  label = "Label",
+  type = "text",
+  value = "",
+  onChange,
+}) {
+  const id = useId();
+
+  const handleChange = (e) => onChange(e.target.value);
+
+  return (
+    <div>
+      <label className="form-label" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        placeholder={placeholder}
+        className="form-control"
+        onChange={handleChange}
+        value={value}
+        type={type}
+        id={id}
+      />
+    </div>
+  );
+}
+
+function createColumnDefs(rows) {
+  const firstRow = Array.isArray(rows) ? rows[0] : {};
+
+  const fields = Object.keys(firstRow);
+
+  return fields.map((field) => ({ field }));
 }
